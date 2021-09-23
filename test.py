308cc35f0a8f09292410ab24c0bbf9171b01a526
Jose Videla <jvidealaolmos@gmail.com>
2021-09-23 01:06:19 -0300

    .

--- test.py
+++ test.py
@@ -1,24 +1,9 @@
-from checkov.common.models.enums import CheckResult, CheckCategories
-from checkov.terraform.checks.resource.base_resource_check import BaseResourceCheck
-
-class variables(BaseResourceCheck):
-    def __init__(self):
-        name = "Ensure all data stored in the RDS is securely encrypted at rest"
-        id = "CKV_AWS_16"
-        supported_resources = ['variable']
-        categories = [CheckCategories.GENERAL_SECURITY]
-        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
-
-def scan_resource_conf(self, conf, entity_type):
-    """
-        Looks for encryption configuration at aws_db_instance:
-        https://www.terraform.io/docs/providers/aws/d/db_instance.html
-    :param conf: aws_db_instance configuration
-    :return: <CheckResult>
-    """
-    if 'type' in conf.keys():
-      return CheckResult.PASSED
-
-    return CheckResult.FAILED
-
-check = variables()
+metadata:
+  name: "Check that all resources are tagged with the key - env"
+  id: "CKV2_AWS_1"
+  category: "GENERAL_SECURITY"
+definition:
+  cond_type: "attribute"
+  resource_types: "all"
+  attribute: "tags"
+  operator: "exists"
